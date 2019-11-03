d3e29c9ddf44344502b7429aeb6cd8bc9f80bd05
Iddan Aaronsohn <mail@aniddan.com>
2019-11-03 19:37:03 +0200

    Pages for recovery and tests

---
+++ pages/conditions/[name]/recovery.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Recovery from "../../../components/recovery";
+
+const RecoveryPage = () => {
+  return (
+    <div>
+      <Recovery />
+    </div>
+  );
+};
+
+export default RecoveryPage;
---
+++ pages/conditions/[name]/tests.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Tests from "../../../components/tests";
+
+const TestsPage = () => {
+  return (
+    <div>
+      <Tests />
+    </div>
+  );
+};
+
+export default TestsPage;
